438df3a064457135e92a59f0605b07187ec010f9
Jon Gjengset <jon@thesquareplanet.com>
2016-10-28 12:10:17 -0400

    Don't run apply-usr if not ready

--- apply.sh
+++ apply.sh
@@ -4,4 +4,9 @@ patch -Np1 gitmail post-receive-email.patch
 mv gitmail /usr/bin/gitmail
 chmod +x /usr/bin/gitmail
 
+if ! id archlinux; then
+	echo "still in setup mode -- not running apply-usr"
+	exit 0
+fi
+
 sudo -u archlinux ./apply-usr.sh "$@"
