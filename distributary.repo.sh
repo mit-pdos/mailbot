d30748470e5ec5429853331f1954f7ab6446ada2
Jon Gjengset <jon@thesquareplanet.com>
2016-10-28 02:06:50 -0400

    Avoid extra space

--- distributary.repo.sh
+++ distributary.repo.sh
@@ -3,7 +3,7 @@ configure() {
 	git config hooks.mailinglist jon@thesquareplanet.com
 	#git config hooks.announcelist
 	#git config hooks.envelopesender
-	git config hooks.emailprefix "[soup] "
+	git config hooks.emailprefix "[soup]"
 	#git config hooks.showrev
 	#git config hooks.emailmaxlines
 	#git config hooks.diffopts
--- hooks.repo.sh
+++ hooks.repo.sh
@@ -3,7 +3,7 @@ configure() {
 	git config hooks.mailinglist jon@tsp.io
 	#git config hooks.announcelist
 	#git config hooks.envelopesender
-	git config hooks.emailprefix "[hooks] "
+	git config hooks.emailprefix "[hooks]"
 	#git config hooks.showrev
 	#git config hooks.emailmaxlines
 	#git config hooks.diffopts
