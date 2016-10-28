137cf1762865e24c53987a62081182e9437e0827
Jon Gjengset <jon@thesquareplanet.com>
2016-10-27 21:04:23 -0400

    First draft

---
+++ apply.sh
@@ -0,0 +1,102 @@
+#!/usr/bin/bash
+json="/srv/hooks.json"
+secret=$(cat /persist/deploy-secret)
+
+echo "[" > "$json"
+for f in *.repo.sh; do
+	if [ "$f" = "*.repo.sh" ]; then
+		break;
+	fi
+
+	name="${f%.repo.sh}"
+	path="/srv/git/$name"
+
+	# .repo file is expected to
+	#  - set $url
+	#  - define configure() function for git configs
+	. "$f"
+	if [ -z "$url" ]; then
+		echo "no url specified for repo $name -- skipping"
+		continue;
+	fi
+
+	if [ -e "$path" ]; then
+		# check that it's in the right state
+		if [ ! -d "$path" ]; then
+			echo "found non-directory repo checkout at $path"
+			#rm -rf "$path"
+		else
+			curl=$(git -C config --get remote.origin.url)
+			if [ "$curl" != "$url" ]; then
+				echo "$path is a check-out of other url $curl"
+				#rm -rf "$path"
+			fi
+		fi
+	fi
+	if [ ! -e "$path" ]; then
+		echo "creating new checkout for $url"
+		git clone --bare "$url" "$path"
+	fi
+
+	# various calls to git config
+	pushd "$path"
+	export GIT_DIR="$path"
+	configure
+	popd
+
+	# add to hooks.json
+	cat <<EOF
+  {
+    "id": "email-$name",
+    "execute-command": "/bin/mailer",
+    "command-working-directory": "/root"
+    "pass-arguments-to-command":
+    [
+      {
+        "source": "string",
+        "name": "$path"
+      },
+      {
+        "source": "payload",
+        "name": "ref"
+      },
+      {
+        "source": "payload",
+        "name": "before"
+      },
+      {
+        "source": "payload",
+        "name": "after"
+      }
+    ],
+    "trigger-rule":
+    {
+      "and":
+      [
+        {
+          "match":
+          {
+            "type": "payload-hash-sha1",
+            "secret": "$secret",
+            "parameter":
+            {
+              "source": "header",
+              "name": "X-Hub-Signature"
+            }
+          }
+        }
+      ]
+    }
+  }
+EOF
+	echo ","
+done >> "$json"
+
+# remove last comma, because javascript
+sed -i '$ d' "$json"
+
+# this is a list
+echo "]" >> "$json"
+
+# restart webhook
+pkill -USR1 webhook
