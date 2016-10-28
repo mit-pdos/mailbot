8893dc93a5af244893c80d43360610cc35ee898d
Tej Chajed <tchajed@mit.edu>
2016-10-28 15:57:19 -0400

    Add FSCQ repos

---
+++ fscq-doc.repo.sh
@@ -0,0 +1,5 @@
+url="git@github.com:mit-pdos/fscq-doc.git"
+configure() {
+    git config hooks.mailinglist
+    git config hooks.emailprefix "[fscq-doc]"
+}
---
+++ fscq-impl.repo.sh
@@ -0,0 +1,5 @@
+url="git@github.com:mit-pdos/fscq-impl.git"
+configure() {
+    git config hooks.mailinglist
+    git config hooks.emailprefix "[fscq-impl]"
+}
