9cfc1955bdc6725eb9019792be2c41bfdc75be10
Jon Gjengset <jon@thesquareplanet.com>
2016-10-28 02:13:24 -0400

    Autopatch gitmail when appropriate

--- apply.sh
+++ apply.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+wget -O gitmail https://raw.githubusercontent.com/git/git/master/contrib/hooks/post-receive-email
+patch -Np1 gitmail post-receive-email.patch
+mv gitmail /usr/bin/gitmail
+chmod +x /usr/bin/gitmail
+
 sudo -u archlinux ./apply-usr.sh "$@"
