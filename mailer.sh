1ef530847e3c141d5433e852279bebd6e971f85f
Jon Gjengset <jon@thesquareplanet.com>
2016-10-28 17:34:23 -0400

    Better output for mail hook

--- mailer.sh
+++ mailer.sh
@@ -8,6 +8,9 @@ pusher="$5"
 email="$6"
 from="$pusher <$email>"
 
+echo "handling push to $repo"
+echo "pusher: $from"
+
 alias=$(grep -E "^$pusher " "/srv/external/wc/mailbot/alias")
 #alias=$(grep -E "^$pusher " "./alias")
 if [ -n "$alias" ]; then
@@ -21,12 +24,15 @@ if [ -n "$alias" ]; then
 	else
 		echo "Invalid alias '$alias' for username $pusher; ignoring"
 	fi
+	echo " -> $from"
 fi
 
+echo "fetching upstead"
 export GIT_DIR="$repo"
 cd "$repo"
 git fetch --all
 
+echo "invoking post-receive-email"
 # todo: migrate to multimail?
 # https://github.com/git/git/blob/master/contrib/hooks/multimail/README.migrate-from-post-receive-email
 echo "$before" "$after" "$ref" | /usr/bin/gitmail "$from" 2>&1
