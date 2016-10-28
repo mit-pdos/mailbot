074adb54c2d0ff602b3b17d4c43c5ff8b37ecbe6
Jon Gjengset <jon@thesquareplanet.com>
2016-10-28 12:16:59 -0400

    Get rid of some test things

--- hooks.repo.sh
+++ hooks.repo.sh
@@ -1,9 +1,8 @@
 url="git@github.com:mit-pdos/githooks.git"
 configure() {
 	git config hooks.mailinglist jon@tsp.io
-	#git config hooks.announcelist
-	#git config hooks.envelopesender
 	git config hooks.emailprefix "[hooks]"
+	#git config hooks.announcelist
 	#git config hooks.showrev
 	#git config hooks.emailmaxlines
 	#git config hooks.diffopts
