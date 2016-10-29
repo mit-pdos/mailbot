041822300bf4d3d17a6eedd8757b893742414f3d
Frans Kaashoek <kaashoek@mit.edu>
2016-10-29 13:11:05 -0400

    x

--- repositories/distributary.sh
+++ repositories/distributary.sh
@@ -1,5 +1,5 @@
 url="git@github.com:mit-pdos/distributary.git"
 configure() {
-    git config hooks.mailinglist malte@csail.mit.edu,nikhil.benesch@gmail.com
+    git config hooks.mailinglist malte@csail.mit.edu,nikhil.benesch@gmail.com,kaashoek@mit.edu
     git config hooks.emailprefix "[distributary]"
 }
