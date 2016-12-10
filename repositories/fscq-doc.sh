f9a2889236b18a70ee11aba98e306dcc0ca5570b
Tej Chajed <tchajed@mit.edu>
2016-12-10 11:36:55 -0500

    Avoid duplicate email notifications for FSCQ repos

--- repositories/fscq-doc.sh
+++ repositories/fscq-doc.sh
@@ -1,5 +1,6 @@
 url="git@github.com:mit-pdos/fscq-doc.git"
 configure() {
-    git config hooks.mailinglist pdos-fscq-doc@lists.csail.mit.edu
+    #git config hooks.mailinglist pdos-fscq-doc@lists.csail.mit.edu
+    git config hooks.mailinglist tchajed@mit.edu
     git config hooks.emailprefix "[fscq-doc]"
 }
--- repositories/fscq-impl.sh
+++ repositories/fscq-impl.sh
@@ -1,5 +1,6 @@
-url="git@github.com:mit-pdos/fscq-impl.git"
+url="git@github.com:mit-pdos/fscq.git"
 configure() {
-    git config hooks.mailinglist pdos-fscq-impl@lists.csail.mit.edu
+    #git config hooks.mailinglist pdos-fscq-impl@lists.csail.mit.edu
+    git config hooks.mailinglist tchajed@mit.edu
     git config hooks.emailprefix "[fscq-impl]"
 }
