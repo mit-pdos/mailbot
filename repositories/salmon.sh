50e952a948bedf23c38122d177c8864d07dfed49
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-07-17 14:27:21 -0400

    Conclave/Salmon collaborators

--- repositories/salmon.sh
+++ repositories/salmon.sh
@@ -1,5 +1,5 @@
 url="git@github.com:n1v0lg/salmon.git"
 configure() {
-	git config hooks.mailinglist malte@csail.mit.edu
-	git config hooks.emailprefix "[salmon]"
+  git config hooks.mailinglist malte@csail.mit.edu, nikolaj@bu.edu, bengetch@gmail.com
+  git config hooks.emailprefix "[salmon]"
 }
