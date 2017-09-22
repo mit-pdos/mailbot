30f04c7aad8d6e5e8a2af7615d64156ae883bd77
Tej Chajed <tchajed@mit.edu>
2017-09-22 17:39:47 -0400

    Add 6.826 released labs repo

---
+++ repositories/6.826-2017-labs.sh
@@ -0,0 +1,5 @@
+url="git@github.com:mit-pdos/6.826-2017-labs.git"
+configure() {
+    git config hooks.mailinglist 6826-staff@lists.csail.mit.edu
+    git config hooks.emailprefix "[6.826 2017 Labs]"
+}
