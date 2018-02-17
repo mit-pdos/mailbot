4a456a6efac8ac85ed3f9bb2f1e9b3dbbc4659ca
David Lazar <lazard@csail.mit.edu>
2018-02-17 13:42:47 -0500

    Add alpenhorn repo

---
+++ repositories/alpenhorn.sh
@@ -0,0 +1,5 @@
+url="git@github.com:vuvuzela/alpenhorn.git"
+configure() {
+  git config hooks.mailinglist lazard@csail.mit.edu,nickolai@csail.mit.edu
+  git config hooks.emailprefix "[alpenhorn]"
+}
