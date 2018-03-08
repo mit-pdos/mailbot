b6a009f7c847e9829876572b10416f2d52016851
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2018-03-08 00:36:54 -0500

    Same recipients as for distributary

--- repositories/distributary-mysql.sh
+++ repositories/distributary-mysql.sh
@@ -1,5 +1,5 @@
 url="git@github.com:mit-pdos/distributary-mysql.git"
 configure() {
-    git config hooks.mailinglist malte@csail.mit.edu
+    git config hooks.mailinglist malte@csail.mit.edu,kaashoek@mit.edu,rtm@csail.mit.edu
     git config hooks.emailprefix "[distributary-mysql]"
 }
