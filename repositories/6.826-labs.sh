a4d829062540479adec01f6f66a8a8807c50f6d0
Tej Chajed <tchajed@mit.edu>
2018-03-29 10:05:50 -0400

    Add Adam to 6.826 mailing list
    for work on concur2 branch

--- repositories/6.826-labs.sh
+++ repositories/6.826-labs.sh
@@ -1,5 +1,5 @@
 url="git@github.com:mit-pdos/6.826-labs.git"
 configure() {
-    git config hooks.mailinglist tchajed@mit.edu,kaashoek@mit.edu,nickolai@csail.mit.edu
+    git config hooks.mailinglist tchajed@mit.edu,kaashoek@mit.edu,nickolai@csail.mit.edu,adamc@csail.mit.edu
     git config hooks.emailprefix "[6.826 Labs]"
 }
