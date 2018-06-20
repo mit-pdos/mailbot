e8587b9c838551036e05b6410ba34dde169ec212
Tej Chajed <tchajed@mit.edu>
2018-06-20 12:53:57 -0400

    Lower-case cspec in emails
    No need to shout

--- repositories/cspec.sh
+++ repositories/cspec.sh
@@ -1,5 +1,5 @@
 url="git@github.com:mit-pdos/cspec.git"
 configure() {
     git config hooks.mailinglist tchajed@mit.edu,kaashoek@mit.edu,nickolai@csail.mit.edu,adamc@csail.mit.edu
-    git config hooks.emailprefix "[CSPEC]"
+    git config hooks.emailprefix "[cspec]"
 }
