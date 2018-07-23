5a13cc41d9e9bb30c0039f7889a3179daeeefac1
Tej Chajed <tchajed@mit.edu>
2018-07-23 18:49:47 -0400

    Migrate CSPEC to git-mailbot

--- repositories/cspec.sh
+++
@@ -1,5 +0,0 @@
-url="git@github.com:mit-pdos/cspec.git"
-configure() {
-    git config hooks.mailinglist tchajed@mit.edu,kaashoek@mit.edu,nickolai@csail.mit.edu,adamc@csail.mit.edu,gibsons@mit.edu
-    git config hooks.emailprefix "[cspec]"
-}
