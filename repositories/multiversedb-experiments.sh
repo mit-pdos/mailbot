f8e20269824ce2f919cf457da1f771dffd4d2dcf
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2019-02-23 11:26:10 -0500

    add multiversedb-experiments repo

---
+++ repositories/multiversedb-experiments.sh
@@ -0,0 +1,5 @@
+url="git@github.com:mit-pdos/multiversedb-experiments.git"
+configure() {
+    git config hooks.mailinglist malte@csail.mit.edu
+    git config hooks.emailprefix "[multiversedb-experiments]"
+}
