5cf19b732e4f8ba2bb852224044421740ac8436f
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2019-03-01 15:29:13 -0500

    Email notifications for Samyu

--- repositories/noria.sh
+++ repositories/noria.sh
@@ -1,5 +1,5 @@
 url="git@github.com:mit-pdos/noria.git"
 configure() {
-    git config hooks.mailinglist malte@csail.mit.edu,kaashoek@mit.edu,rtm@csail.mit.edu
+    git config hooks.mailinglist malte@csail.mit.edu,kaashoek@mit.edu,rtm@csail.mit.edu,samyu@mit.edu
     git config hooks.emailprefix "[noria]"
 }
