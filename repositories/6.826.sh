ecaea78c3b893e999547aa4e40a7b6ef5a7c274d
Nickolai Zeldovich <nickolai@csail.mit.edu>
2019-09-02 16:50:09 -0400

    update 6.826 staff emails

--- repositories/6.826.sh
+++ repositories/6.826.sh
@@ -1,5 +1,5 @@
 url="git@github.com:mit-pdos/6.826.git"
 configure() {
-    git config hooks.mailinglist tchajed@mit.edu,kaashoek@mit.edu,nickolai@csail.mit.edu
+    git config hooks.mailinglist tchajed@mit.edu,nickolai@csail.mit.edu,atalay@mit.edu
     git config hooks.emailprefix "[6.826]"
 }
