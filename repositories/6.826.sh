f309371bc67f4a036e7e31ac5e59bedbc7ecfa11
Tej Chajed <tchajed@mit.edu>
2019-09-07 10:21:24 -0400

    Remove 6.826

--- repositories/6.826.sh
+++
@@ -1,5 +0,0 @@
-url="git@github.com:mit-pdos/6.826.git"
-configure() {
-    git config hooks.mailinglist tchajed@mit.edu,nickolai@csail.mit.edu,atalay@mit.edu
-    git config hooks.emailprefix "[6.826]"
-}
