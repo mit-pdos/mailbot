b1604f4bb95c38b8c9f1ed614e2e3ac40394ee02
Nickolai Zeldovich <nickolai@csail.mit.edu>
2020-05-18 12:14:47 -0400

    ward_paper.sh

---
+++ repositories/ward_paper.sh
@@ -0,0 +1,6 @@
+url="git@github.com:mit-pdos/ward_paper.git"
+configure() {
+    #git config hooks.mailinglist pdos-fscq-doc@lists.csail.mit.edu
+    git config hooks.mailinglist behrensj@mit.edu,abelay@mit.edu,kaashoek@mit.edu,nickolai@csail.mit.edu
+    git config hooks.emailprefix "[ward_paper]"
+}
